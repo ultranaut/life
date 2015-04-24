3c7852f492c6dd95f984b6191526d31a2171f4e5
John Shearer <john@ultranaut.com>
2015-04-24 11:11:29 -0400

    Minor tweaks to main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 /* global Life */
-var cellColor = '#b58900';
-var graveColor = '#586e75';
+var cellColor   = '#b58900';
+var graveColor  = '#586e75';
 var canvasColor = '#073642';
 
 var game = new Life(600, 450, 5);
-game.canvas.style.backgroundColor = this.checked ? graveColor : canvasColor;
 game.setColor('cellColor', cellColor);
 game.setColor('canvasColor', canvasColor);
-game.place('glider', 13, 1);
-game.place('gosper', 22, 5);
-game.place('gosper', 5, 22);
+game.place('glider', 13,  1);
+game.place('gosper', 22,  5);
+game.place('gosper',  5, 22);
 game.place('gosper', 78, 45);
 game.place('gosper', 73, 10);
 game.place('pulsar', 10, 67);
 
 document.getElementById('life').appendChild(game.canvas);
+game.canvas.style.backgroundColor = canvasColor;
 
 document.getElementById('showGraves').onchange = function (e) {
   game.canvas.style.backgroundColor = this.checked ? graveColor : canvasColor;
