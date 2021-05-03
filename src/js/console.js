0652818edce69c2ec47d2cd9c6df31787c651350
John Shearer <john@ultranaut.com>
2021-05-03 14:18:45 -0400

    Modernize `Console` and `Life` modules
    ES6 class, const, let, etc

--- src/js/console.js
+++ src/js/console.js
@@ -6,76 +6,76 @@
  * @param {number} cellSize - The size of a cell in pixels.
  * @returns {Node} An HTML canvas node
  */
-function Console(width, height, cellSize) {
-  // set instance properties
-  this.width = width / cellSize; // width and height of grid in cells
-  this.height = height / cellSize;
-  this.cell = cellSize; // size of cells in pixels
+class Console {
+  constructor(width, height, cellSize) {
+    // width and height of grid in cells
+    this.width = width / cellSize;
+    this.height = height / cellSize;
+    // size of cells in pixels
+    this.resolution = cellSize;
 
-  this.matrix = []; // matrix of cell states
+    this.matrix = []; // matrix of cell states
+    this.cellColor = '#000';
 
-  this.cellColor = '#000';
+    this.createCanvas();
+    this._initializeMatrix();
+  }
 
-  // create an HTML canvas element; note we're using the args
-  //   values here still since they're in pixels.
-  this.canvas = document.createElement('canvas');
-  this.canvas.width = width;
-  this.canvas.height = height;
-  this.context = this.canvas.getContext('2d');
+  // create an HTML canvas elemen
+  createCanvas() {
+    this.canvas = document.createElement('canvas');
+    this.canvas.width = this.width * this.resolution;
+    this.canvas.height = this.height * this.resolution;
+    this.context = this.canvas.getContext('2d');
+  }
 
-  _initializeMatrix.call(this);
-}
+  setColor(color) {
+    this.cellColor = color;
+  }
 
-Console.prototype.setColor = function (color) {
-  this.cellColor = color;
-};
+  refresh(data) {
+    this._draw(data);
+    this.matrix = data;
+  }
 
-Console.prototype.refresh = function (data) {
-  _draw.call(this, data);
-  this.matrix = data;
-};
+  /* ********************* Private methods ************************ */
 
-/* ********************* Private methods ************************ */
+  /*
+   * Create the cell matrix.
+   *
+   * Sets _matrix to an n x m array with all values initialized to 0
+   */
+  _initializeMatrix() {
+    const row = Array(this.width).fill(0);
+    this.matrix = Array(this.height).fill(row.slice(0));
+  }
 
-/*
- * Create the cell matrix.
- *
- * Sets _matrix to an n x m array with all values initialized to 0
- */
-var _initializeMatrix = function () {
-  var starterRow = Array.apply(null, new Array(this.width)).map(function () {
-    return 0;
-  });
-  this.matrix = Array.apply(null, new Array(this.height)).map(function () {
-    return starterRow.slice(0);
-  });
-};
+  /*
+   * Translate matrix onto the canvas.
+   */
+  _draw(data) {
+    let m, n, x, y, currentValue, newValue;
+    const resolution = this.resolution;
+    const matrix = this.matrix;
+    const context = this.context;
 
-/*
- * Translate matrix onto the canvas.
- */
-var _draw = function (data) {
-  var m, n, x, y, currentValue, newValue;
-  var cell = this.cell;
-  var matrix = this.matrix;
-  var context = this.context;
-
-  for (n = 0; n < this.height; n++) {
-    for (m = 0; m < this.width; m++) {
-      x = m * cell;
-      y = n * cell;
-      currentValue = matrix[n][m];
-      newValue = data[n][m];
-      if (newValue !== currentValue) {
-        if (newValue === 1) {
-          context.fillStyle = this.cellColor;
-          context.fillRect(x, y, cell, cell);
-        } else {
-          context.clearRect(x, y, cell, cell);
+    for (n = 0; n < this.height; n++) {
+      for (m = 0; m < this.width; m++) {
+        x = m * resolution;
+        y = n * resolution;
+        currentValue = matrix[n][m];
+        newValue = data[n][m];
+        if (newValue !== currentValue) {
+          if (newValue === 1) {
+            context.fillStyle = this.cellColor;
+            context.fillRect(x, y, resolution, resolution);
+          } else {
+            context.clearRect(x, y, resolution, resolution);
+          }
         }
       }
     }
   }
-};
+}
 
 export default Console;
--- src/js/life.js
+++ src/js/life.js
@@ -1,4 +1,4 @@
-var events = {};
+const events = {};
 
 /**
  * Creates a Game of Life object.
@@ -7,146 +7,144 @@ var events = {};
  * @param {number} height - The number of rows.
  * @param {boolean} [wrap=true]  - Wrap the  canvas.
  */
-function Life(width, height, wrap) {
-  // set instance properties
-  this.width = width; // width and height of grid in cells
-  this.height = height;
-  this.wrap = typeof wrap === 'undefined' ? true : !!wrap;
+class Life {
+  constructor(width, height, wrap) {
+    // width and height of grid in cells
+    this.width = width;
+    this.height = height;
+    this.wrap = typeof wrap === 'undefined' ? true : !!wrap;
 
-  this.matrix = null; // matrix of cell states;
-  this.generation = 120; // life of a generation in milliseconds
-  this.intervalId = null; // interval id of start function
+    this.matrix = null; // matrix of cell states;
+    this.generation = 120; // life of a generation in milliseconds
+    this.intervalId = null; // interval id of start function
 
-  this.initializeMatrix();
+    this.initializeMatrix();
+  }
+
+  /*
+   * Create or reset the cell matrix.
+   *
+   * Sets _matrix to an n x m array with all values initialized to 0
+   */
+  initializeMatrix() {
+    const row = Array(this.width).fill(0);
+    this.matrix = Array.apply(null, new Array(this.height)).map(function () {
+      return row.slice(0);
+    });
+  }
+
+  /**
+   * Place an object into the grid
+   *
+   * @param {string} pattern - Key of pattern to use.
+   * @param {number} x - The x-coordinate of the placement.
+   * @param {number} y - The y-coordinate of the placement.
+   */
+  place(pattern, x, y) {
+    const schema = this.patterns[pattern];
+
+    for (let n = 0; n < schema.length; n++) {
+      for (let m = 0; m < schema[0].length; m++) {
+        this.matrix[n + y][m + x] = schema[n][m];
+      }
+    }
+    // this.trigger('refresh', [this.matrix]);
+  }
+
+  on(event, fn) {
+    events[event] = events[event] || [];
+    events[event].push(fn);
+  }
+
+  trigger(event, args) {
+    events[event] = events[event] || [];
+    args = args || [];
+    events[event].forEach(function (fn) {
+      fn.apply(this, args);
+    });
+  }
+  /*
+   * Generate the next, uhh, generation.
+   */
+  step() {
+    let n, m, neighbors;
+    const nextGen = this._cloneMatrix.call(this);
+
+    for (n = 0; n < this.height; n++) {
+      for (m = 0; m < this.width; m++) {
+        neighbors = this._countNeighbors.call(this, n, m);
+        if (neighbors < 2 || neighbors > 3) {
+          nextGen[n][m] = 0;
+        } else if (neighbors === 3) {
+          nextGen[n][m] = 1;
+        }
+      }
+    }
+    this.matrix = nextGen;
+    this.trigger('refresh', [this.matrix]);
+    return this;
+  }
+
+  /**
+   * Start iterating generations.
+   */
+  start() {
+    this.intervalId = setInterval(this.step.bind(this), this.generation);
+  }
+
+  /**
+   * Stop iterating generations.
+   */
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  /**
+   * @returns {boolean}
+   */
+  isRunning() {
+    return this.intervalId !== null;
+  }
+
+  /* ********************* Private methods ************************ */
+
+  /*
+   * Produce a clone of the _matrix.
+   */
+  _cloneMatrix() {
+    const retval = [];
+    for (let i = 0; i < this.matrix.length; i++) {
+      retval[i] = this.matrix[i].slice(0);
+    }
+    return retval;
+  }
+
+  /*
+   * Count the number of neighbors;
+   */
+  _countNeighbors(row, col) {
+    // Don't want to count the current cell itself.
+    let pop = 0 - this.matrix[row][col];
+    let i, j;
+
+    for (i = -1; i < 2; i++) {
+      for (j = -1; j < 2; j++) {
+        if (this.wrap === true) {
+          pop += this.matrix[(this.height + row + i) % this.height][
+            (this.width + col + j) % this.width
+          ];
+        } else if (
+          typeof this.matrix[row + i] !== 'undefined' &&
+          typeof this.matrix[row + i][col + j] !== 'undefined'
+        ) {
+          pop += this.matrix[row + i][col + j];
+        }
+      }
+    }
+    return pop;
+  }
 }
-
-/*
- * Create or reset the cell matrix.
- *
- * Sets _matrix to an n x m array with all values initialized to 0
- */
-Life.prototype.initializeMatrix = function () {
-  // creates an array of `width` 0's
-  var row = Array.apply(null, new Array(this.width)).map(function () {
-    return 0;
-  });
-  // creates and array of `height` rows
-  this.matrix = Array.apply(null, new Array(this.height)).map(function () {
-    return row.slice(0);
-  });
-};
-
-/**
- * Place an object into the grid
- *
- * @param {string} pattern - Key of pattern to use.
- * @param {number} x - The x-coordinate of the placement.
- * @param {number} y - The y-coordinate of the placement.
- */
-Life.prototype.place = function (pattern, x, y) {
-  var schema = this.patterns[pattern];
-
-  for (var n = 0; n < schema.length; n++) {
-    for (var m = 0; m < schema[0].length; m++) {
-      this.matrix[n + y][m + x] = schema[n][m];
-    }
-  }
-  // this.trigger('refresh', [this.matrix]);
-};
-
-Life.prototype.on = function (event, fn) {
-  events[event] = events[event] || [];
-  events[event].push(fn);
-};
-Life.prototype.trigger = function (event, args) {
-  events[event] = events[event] || [];
-  args = args || [];
-  events[event].forEach(function (fn) {
-    fn.apply(this, args);
-  });
-};
-
-/*
- * Generate the next, uhh, generation.
- */
-Life.prototype.step = function () {
-  var n, m, neighbors;
-  var nextGen = _cloneMatrix.call(this);
-
-  for (n = 0; n < this.height; n++) {
-    for (m = 0; m < this.width; m++) {
-      neighbors = _countNeighbors.call(this, n, m);
-      if (neighbors < 2 || neighbors > 3) {
-        nextGen[n][m] = 0;
-      } else if (neighbors === 3) {
-        nextGen[n][m] = 1;
-      }
-    }
-  }
-  this.matrix = nextGen;
-  this.trigger('refresh', [this.matrix]);
-  return this;
-};
-
-/**
- * Start iterating generations.
- */
-Life.prototype.start = function () {
-  this.intervalId = setInterval(this.step.bind(this), this.generation);
-};
-
-/**
- * Stop iterating generations.
- */
-Life.prototype.stop = function () {
-  clearInterval(this.intervalId);
-  this.intervalId = null;
-};
-
-/**
- * @returns {boolean}
- */
-Life.prototype.isRunning = function () {
-  return this.intervalId !== null;
-};
-
-/* ********************* Private methods ************************ */
-
-/*
- * Produce a clone of the _matrix.
- */
-var _cloneMatrix = function () {
-  var retval = [];
-  for (var i = 0; i < this.matrix.length; i++) {
-    retval[i] = this.matrix[i].slice(0);
-  }
-  return retval;
-};
-
-/*
- * Count the number of neighbors;
- */
-var _countNeighbors = function (row, col) {
-  // Don't want to count the current cell itself.
-  var pop = 0 - this.matrix[row][col];
-  var i, j;
-
-  for (i = -1; i < 2; i++) {
-    for (j = -1; j < 2; j++) {
-      if (this.wrap === true) {
-        pop += this.matrix[(this.height + row + i) % this.height][
-          (this.width + col + j) % this.width
-        ];
-      } else if (
-        typeof this.matrix[row + i] !== 'undefined' &&
-        typeof this.matrix[row + i][col + j] !== 'undefined'
-      ) {
-        pop += this.matrix[row + i][col + j];
-      }
-    }
-  }
-  return pop;
-};
 
 /*
  * Some standard patterns.
